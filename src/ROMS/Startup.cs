f8217b4245fb2d258a11d058e37f5534071b5a86
AYIV <an.nk.ivanov@outlook.com>
2015-09-27 07:53:36 -0700

    Solution setup: 1. Added: 1.1. npm packages support 1.2. grunt 1.3. bower 1.4. jquery bower package 1.5. mvc support 1.6. empty home controller & view

--- src/ROMS/Startup.cs
+++ src/ROMS/Startup.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.Framework.DependencyInjection;
 
 namespace ROMS
@@ -13,13 +8,16 @@ namespace ROMS
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
